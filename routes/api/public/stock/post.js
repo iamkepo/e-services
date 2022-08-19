b2466aa1dd950fcadcea69fa52df04a48e06acd5
iamkepo <christamour.kakpo@gmail.com>
2022-08-19 15:36:17 +0200

    email verify

--- routes/api/public/stock/post.js
+++ routes/api/public/stock/post.js
@@ -3,12 +3,24 @@ var router = express.Router();
 var connect = require('../../../../helper/connect');
 
 router.post('/', async (req, res) => {
+  if (req.body.table == "mails") {
+    connect.db.collection(req.body.table)
+    .findOne({email: req.body.data.email})
+    .then((response)=> {
+      if (response == null) {
+        connect.db.collection(req.body.table)
+        .insertOne(req.body.data)
+        .then(()=>{
+          res.json({message: "cet email a été inscrit avec succès"});
+        });
+      } else {
+        res.json({message: "cet email a été déjà inscrit"});
+      }
+    });
+  } else {
+    res.json({message: "table invalid"});
+  }
   
-  connect.db.collection(req.body.table)
-  .insertOne(req.body.data)
-  .then((response)=>{
-    res.json(response);
-  });
   
 });
 

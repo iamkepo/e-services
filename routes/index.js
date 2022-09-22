55baf4f4392e22238cac1cae269863dda354de5b
iamkepo <christamour.kakpo@gmail.com>
2022-09-22 10:56:59 +0200

    route refactory

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const page = (app) => {
+  app.use('/', require('./page/index'));
+};
+
+const api = (app) => {
+  app.use('/api/public/stock/post', require('./api/public/stock/post'));
+  app.use('/api/public/icon/group/get', require('./api/public/icon/group/get'));
+  app.use('/api/public/icon/get', require('./api/public/icon/get'));
+  app.use('/api/public/pic', require('./api/public/pic'));
+  app.use('/api/public/pics', require('./api/public/pics'));
+  app.use('/api/private/auth/register', require('./api/private/auth/register'));
+  app.use('/api/private/auth/login', require('./api/private/auth/login'));
+  app.use('/api/private/auth/refreshToken', require('./api/private/auth/refreshToken'));
+  app.use('/api/private/user/get', require('./api/private/user/get'));
+  app.use('/api/private/user/me/get', require('./api/private/user/me/get'));
+  app.use('/api/private/icon/create', require('./api/private/icon/create'));
+};
+
+module.exports = { page, api };
--- server.js
+++ server.js
@@ -16,28 +16,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //api routes import
-var route = require('./routes/route');
+var route = require('./routes');
 
 
 
 
-//page
-app.use('/', route.page.index);
- 
-//api public
-app.use('/api/public/stock/post', route.api.public.poststock),
-app.use('/api/public/group/icon', route.api.public.groupicon),
-app.use('/api/public/icon', route.api.public.icon),
-app.use('/api/public/pics', route.api.public.pics),
-app.use('/api/public/pic', route.api.public.pic),
+var route = require('./routes/index');
 
-//api private
-app.use('/api/private/auth/register', route.api.private.register);
-app.use('/api/private/auth/login', route.api.private.login);
-app.use('/api/private/auth/refreshToken', route.api.private.refreshToken);
-app.use('/api/private/user/get', route.api.private.user);
-app.use('/api/private/user/me', route.api.private.me);
-app.use('/api/private/icon/create', route.api.private.createicon);
+//start routing
+route.page(app);
+route.api(app);
 
 
 connect.start().then(()=> {

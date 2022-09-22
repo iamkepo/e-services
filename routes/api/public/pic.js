a3335b03c6bff6d26df94ed5a3fe61cf58a611ce
iamkepo <christamour.kakpo@gmail.com>
2022-09-22 02:45:05 +0200

    pic finish

--- routes/api/public/pic.js
+++ routes/api/public/pic.js
@@ -1,22 +1,25 @@
-const SerpApi = require('google-search-results-nodejs');
+const GoogleImages = require('google-images');
 require('dotenv').config();
 
 var express = require('express');
 var router = express.Router();
 
-router.get('/', async (req, res) => {
-
-  const search = new SerpApi.GoogleSearch(process.env.G_API);
-  const params = {
-    q: "Apple",
-    tbm: "isch",
-    ijn: "0"
-  };
-
-  search.json(params, function(data) {
-    res.json(data["images_results"]);
+router.get('/:name/:index', async (req, res) => {
+  const client = new GoogleImages("24eeda5196c3f449b", process.env.G_API);
+	client.search(req.params.name)
+  .then(images => {
+    if (req.params.index === "all") {
+      res.send(images);
+    } else {
+      if (parseInt(req.params.index) < 0) {
+        res.send(images[0].url);
+      } else if (parseInt(req.params.index) > images.length-1) {
+        res.send(images[images.length-1].url)
+      } else {
+        res.send(images[parseInt(req.params.index)].url);
+      }
+    }
   });
-
 });
 
 module.exports = router;

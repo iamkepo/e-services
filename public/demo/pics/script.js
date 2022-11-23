022c15dd58b6fb57fec9c635b19d1f95334913b1
christamour.kakpo <christamour.kakpo@gmail.com>
2022-11-23 15:36:56 +0100

    pics loader test

--- public/demo/pics/script.js
+++ public/demo/pics/script.js
@@ -9,6 +9,7 @@ function search(key) {
   if (key == 'Enter') {
     let query = document.querySelector('.query').value;
     let limit = document.querySelector('.limit').value;
+    document.querySelector('.loader').style.display = "flex";
     axios({
       method: "get",
       url: window.location.origin+'/api/public/pics/'+query+'/'+limit
@@ -20,9 +21,11 @@ function search(key) {
         section = section.concat(`<article class="article"><img src="${el.url}" alt="${el.name}" /></article>`)
       });
       document.querySelector('.section').innerHTML = section;
+      document.querySelector('.loader').style.display = "none";
     })
     .catch((err) => {
       console.log(err);
+      document.querySelector('.loader').style.display = "none";
     });
   }
 }
